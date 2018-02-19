561cf38f6dc2307cbcc8dc1aca9b9e3aa0401f1e
Anil Burak Bilsel <abbilsel@gmail.com>
2018-02-19 12:07:10 -0800

    more info are added

--- Programming Languages/TypeScript/learnTs.ts
+++ Programming Languages/TypeScript/learnTs.ts
@@ -3813,6 +3813,64 @@ dad.name = "Man with the 3-piece suit"; // error! name is readonly.
 // let dad = new Octopus("Man with the 8 strong legs");
 // console.log(dad.name);  //Man with the 8 strong legs
 
+////////////
+
+//accessors
+//TypeScript supports getters/setters as a way of intercepting accesses to a member of an object.
+//getters and setters
+//whenever possible, try to use getters and setters in order to have a more robust code
+
+let passcode = "secret passcode";
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            console.log("Error: Unauthorized update of employee!");
+        }
+    }
+}
+
+let employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    console.log(employee.fullName);
+}
+
+/////////////////
+//There are also static members of the classes.
+//Static members are visible on the class itself rather than on the instances.
+
+class Grid {
+  static origin = {
+    x: 0, 
+    y: 0
+  };
+  calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+      let xDist = (point.x - Grid.origin.x);
+      let yDist = (point.y - Grid.origin.y);
+      return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+  }
+  constructor (public scale: number) { }
+}
+
+let grid1 = new Grid(1.0);  // 1x scale
+let grid2 = new Grid(5.0);  // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
+console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
+
+// In the above example, we use static on the origin, as it’s a general value for all grids. Each instance accesses this value through 
+// prepending the name of the class. Similarly to prepending this. in front of instance accesses, here we prepend Grid. in front of 
+// static accesses.
 
 //////////////////////////////////////////////////////////////
 
