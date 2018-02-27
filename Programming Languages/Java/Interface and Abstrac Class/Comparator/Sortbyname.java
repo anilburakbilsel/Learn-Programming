7d9a2492758dea0996f4754537421d740f114a3d
Anil Burak Bilsel <abbilsel@gmail.com>
2018-02-26 19:32:44 -0800

    abstract class and interfaces

---
+++ Programming Languages/Java/Interface and Abstrac Class/Comparator/Sortbyname.java
@@ -0,0 +1,6 @@
+class Sortbyname implements Comparator<Student> {
+    // Used for sorting in ascending order of roll name
+    public int compare(Student a, Student b) {
+        return a.name.compareTo(b.name);
+    }
+}
---
+++ Programming Languages/Java/Interface and Abstrac Class/Comparator/Sortbyroll.java
@@ -0,0 +1,6 @@
+class Sortbyroll implements Comparator<Student> {
+    // Used for sorting in ascending order of roll number
+    public int compare(Student a, Student b) {
+        return a.rollno - b.rollno;
+    }
+}
