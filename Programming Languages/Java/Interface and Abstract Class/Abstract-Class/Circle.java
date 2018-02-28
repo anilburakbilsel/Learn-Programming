4acdfa41effea61a5bf6b53b2ffb801846aa9620
Anil Burak Bilsel <abbilsel@gmail.com>
2018-02-27 19:15:39 -0800

    a folder for abstraction is added

---
+++ Programming Languages/Java/Interface and Abstract Class/Abstract-Class/Circle.java
@@ -0,0 +1,23 @@
+class Circle extends Shape {
+
+    double pi = 3.14;
+    int radius;
+
+    //constructor
+    Circle(int radius, String name) {
+
+        super(name);
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+
+        System.out.println("Circle has been drawn ");
+    }
+
+    @Override
+    public double area() {
+        return (double) ((pi * radius * radius) / 2);
+    }
+}
