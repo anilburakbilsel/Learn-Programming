d8c7b476eee724a06a6e1c89e910ac2d4c0e3710
anilburakbilsel <abbilsel@gmail.com>
2021-02-10 23:05:19 -0800

    creating and displaying an array in the bash

--- bash/variables.sh
+++ bash/variables.sh
@@ -7,3 +7,14 @@ echo $?                      # displays the exit status of the last command
 read <varname>               # reads a string from the input and assigns it to a variable 
 let <varname> = <equation>   # performs mathematical calculation using operators like +, -, *, /, %
 export VARNAME=value         # defines an environment variable (will be available in subprocesses)
+
+
+array[0]=valA                # how to define an array
+array[1]=valB
+array[2]=valC
+array=([2]=valC [0]=valA [1]=valB)  # another way
+array=(valA valB valC)              # and another
+
+${array[i]}                  # displays array's value for this index. If no index is supplied, array element 0 is assumed
+${#array[i]}                 # to find out the length of any element in the array
+${#array[@]}                 # to find out how many values there are in the array
